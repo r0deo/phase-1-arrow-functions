8056f3c5841e1c9787b23cd3e19c5aaa4c47cfa4
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-02-24 12:42:33 +0300

    completed by Ali

---
+++ index.js
@@ -0,0 +1,20 @@
+// Function expression called divide
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Arrow function called square
+  const square = (num) => {
+    return num * num;
+  };
+  
+  // Arrow function called add
+  const add = (num1, num2) => {
+    return num1 + num2;
+  };
+  
+  // Example usage
+  console.log(divide()); // Should output 20
+  console.log(square(5)); // Should output 25
+  console.log(add(10, 15)); // Should output 25
+  
